542d9d1812be34dd056f2176ee9929a3f5ce8118
Toby Jackson <toby.jackson@futurenet.com>
2014-11-28 16:04:46 +0000

    Cope if commits is nil and mark as unknown commit(s)
    Also include the reponame changes.

--- gitlab-stomp.rb
+++ gitlab-stomp.rb
@@ -26,19 +26,28 @@ post '/' do
 
   case type
   when 'push'
-    reponame = push['repository']['name']
+
+    url = push['repository']['url'].match(/\/(.*)\.git/)
+    reponame = url[1] 
+    #puts "This is a commit for #{project}"
+
     oldrev = push['before']
     newrev = push['after']
     refname = push['ref']
     user = push['user_name']
     repo_homepage = push['repository']['homepage']
+    commit_count = 'unknown'
 
     subject = "[#{reponame}] #{refname} #{newrev}"
     body = "repo: #{reponame}\noldrev: #{oldrev}\nnewrev: #{newrev}\nrefname: #{refname}\n"
 
-    push['commits'].each do |flob|
-      body2 = " commit #{flob['id']}\nAuthor: #{flob['author']['email']}\nDate: #{flob['timestamp']}\n\t#{flob['message']}\n\n"
-      body.concat(body2)
+
+    if !push['commits'].nil?
+      push['commits'].each do |flob|
+        body2 = " commit #{flob['id']}\nAuthor: #{flob['author']['email']}\nDate: #{flob['timestamp']}\n\t#{flob['message']}\n\n"
+        body.concat(body2)
+      end
+      commit_count=commits.length
     end
 
     client = Stomp::Client.new(stompconnector)
@@ -47,7 +56,7 @@ post '/' do
 
       # Create a more human friendly version of the published events that include a url to the latest change
       # Example: John Smith pushed 4 commit(s) to myrepo - http://gitlab.example.org/myrepo/commits/da1560886d4f094c3e6c9ef40349f7d38b5d27d7
-      eventdetail = "#{user} pushed #{push['commits'].length} commit(s) to #{reponame} - #{repo_homepage}/commits/#{newrev} (webhook)"
+      eventdetail = "#{user} pushed #{commit_count} commit(s) to #{reponame} - #{repo_homepage}/commits/#{newrev} (webhook)"
       client.publish("/topic/#{report_topic}",eventdetail, {:subject => "Talking to eventbot"})
       
       Syslog.info("Pushed change: %s",subject)
